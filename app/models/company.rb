023cd8054873b208fed4e0959e0b285e67895204
Joao Daniel <joaodanieldev@gmail.com>
2021-08-30 13:53:26 -0300

    validates no model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,7 @@
 class Company < ApplicationRecord
   has_many :vacancies
+
+  validates :name, presence :true
+  validates :description, length: {minimum:3, maximum:1000},
+            presence :true
 end
--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -1,4 +1,13 @@
 class Vacancy < ApplicationRecord
   belongs_to :company
   has_many :applicants
+
+  validates :title, presence :true
+  validates :location, presence :true
+  validates :description, length: {minimum:3, maximum:1000},
+            presence :true
+  validates :requirements, presence :true
+  validates :salarym presence :true
+  validates :avaible, presence :true
+
 end
